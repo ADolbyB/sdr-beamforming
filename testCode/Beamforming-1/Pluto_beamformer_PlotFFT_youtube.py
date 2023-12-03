4ecba3daee44f555dadd4c40fe8370c3038d2fc0
Joel Brigida <joel@drtechreps.com>
2023-12-03 14:33:46 -0500

    BUGFIX: Correct Plot Loop Drawing New Window Each Iteration

--- testCode/Beamforming-1/Pluto_beamformer_PlotFFT_youtube.py
+++ testCode/Beamforming-1/Pluto_beamformer_PlotFFT_youtube.py
@@ -25,17 +25,6 @@ video walkthrough of this at:  https://www.youtube.com/@jonkraft
 #       patent holders to use this software.
 #     - Use of the software either in source or binary form, must be run
 #       on or directly connected to an Analog Devices Inc. component.
-#
-# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.
-#
-# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
-# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys
 print(f'sys.path = {sys.path}')       # Edit JB: may need to add path to PYTHONPATH for OSError: [Errno 16] Device or resource busy
@@ -43,12 +32,13 @@ print(f'sys.path = {sys.path}')       # Edit JB: may need to add path to PYTHONP
 import adi
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 '''Setup'''
-samp_rate = 2e6    # must be <= 30.72 MHz if both channels are enabled
+samp_rate = 2e6             # must be <= 30.72 MHz if both channels are enabled
 NumSamples = 2**12
 rx_lo = 2.3e9
-rx_mode = "manual"  # can be "manual" or "slow_attack"
+rx_mode = "manual"          # can be "manual" or "slow_attack"
 rx_gain0 = 40
 rx_gain1 = 40
 tx_lo = rx_lo
@@ -68,7 +58,7 @@ sdr.gain_control_mode = rx_mode
 sdr.rx_hardwaregain_chan0 = int(rx_gain0)
 sdr.rx_hardwaregain_chan1 = int(rx_gain1)
 sdr.rx_buffer_size = int(NumSamples)
-sdr._rxadc.set_kernel_buffers_count(1)   # set buffers to 1 (instead of the default 4) to avoid stale data on Pluto
+sdr._rxadc.set_kernel_buffers_count(1)      # set buffers to 1 (instead of the default 4) to avoid stale data on Pluto
 sdr.tx_rf_bandwidth = int(fc0*3)
 sdr.tx_lo = int(tx_lo)
 sdr.tx_cyclic_buffer = True
@@ -84,45 +74,42 @@ t = np.arange(0, N * ts, ts)
 i0 = np.cos(2 * np.pi * t * fc0) * 2 ** 14
 q0 = np.sin(2 * np.pi * t * fc0) * 2 ** 14
 iq0 = i0 + 1j * q0
-sdr.tx([iq0,iq0])  # Send Tx data.
+sdr.tx([iq0,iq0])                           # Send Tx data.
 
-# Assign frequency bins
-xf = np.fft.fftfreq(NumSamples, ts)
+xf = np.fft.fftfreq(NumSamples, ts)         # Assign frequency bins
 xf = np.fft.fftshift(xf)/1e6
 
-def dbfs(raw_data):
-    # function to convert IQ samples to FFT plot, scaled in dBFS
+def dbfs(raw_data):                         # function to convert IQ samples to FFT plot, scaled in dBFS
     NumSamples = len(raw_data)
     win = np.hamming(NumSamples)
     y = raw_data * win
     s_fft = np.fft.fft(y) / np.sum(win)
     s_shift = np.fft.fftshift(s_fft)
-    s_dbfs = 20*np.log10(np.abs(s_shift)/(2**11))     # Pluto is a signed 12 bit ADC, so use 2^11 to convert to dBFS
+    s_dbfs = 20*np.log10(np.abs(s_shift)/(2**11))   # Pluto is a signed 12 bit ADC, so use 2^11 to convert to dBFS
     return s_dbfs
 
 '''Collect Data'''
-for i in range(20):  
-    # let Pluto run for a bit, to do all its calibrations, then get a buffer
+for i in range(20):                         # let Pluto run for a bit, to do all its calibrations, then get a buffer
     data = sdr.rx()
 
 for i in range(1):
     data = sdr.rx()
-    Rx_0=data[0]
-    Rx_1=data[1]
-    
+    Rx_0 = data[0]
+    Rx_1 = data[1]
     peak_sum = []
-    delay_phases = np.arange(-180, 180, 2)    # phase delay in degrees
-    for phase_delay in delay_phases:   
+    delay_phases = np.arange(-180, 180, 2)  # phase delay in degrees
+    for phase_delay in delay_phases:        # plot the FFT of the new delayed signal
         delayed_Rx_1 = Rx_1 * np.exp(1j*np.deg2rad(phase_delay+phase_cal))
         delayed_sum = dbfs(Rx_0 + delayed_Rx_1)
-
-        # plot the FFT of the new delayed signal
+        plt.clf()
         plt.plot(xf, delayed_sum)
         plt.xlabel("frequency [MHz]")
         plt.ylabel("Rx0 + Rx1 [dBfs]")
         plt.ylim(top=0, bottom=-100)
         plt.text(-1, -10, "phase shift = {} deg".format(phase_delay))
         plt.draw()
-        plt.show()
+        plt.pause(0.05)
+        time.sleep(0.1)
+    plt.show()
 
 sdr.tx_destroy_buffer()
