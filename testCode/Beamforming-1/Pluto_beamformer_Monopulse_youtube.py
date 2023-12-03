5f2404f8c1c12d9b1a877cd8637200414757ef0a
Joel Brigida <joel@drtechreps.com>
2023-12-03 14:29:31 -0500

    BUGFIX: Correct Plot Loop Drawing New Window Each Iteration

--- testCode/Beamforming-1/Pluto_beamformer_Monopulse_youtube.py
+++ testCode/Beamforming-1/Pluto_beamformer_Monopulse_youtube.py
@@ -25,49 +25,6 @@ video walkthrough of this at:  https://www.youtube.com/@jonkraft
 #       patent holders to use this software.
 #     - Use of the software either in source or binary form, must be run
 #       on or directly connected to an Analog Devices Inc. component.
-#
-# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.
-#
-# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
-# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# Copyright (C) 2020 Analog Devices, Inc.
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-#     - Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     - Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in
-#       the documentation and/or other materials provided with the
-#       distribution.
-#     - Neither the name of Analog Devices, Inc. nor the names of its
-#       contributors may be used to endorse or promote products derived
-#       from this software without specific prior written permission.
-#     - The use of this software may or may not infringe the patent rights
-#       of one or more patent holders.  This license does not release you
-#       from the requirement that you obtain separate licenses from these
-#       patent holders to use this software.
-#     - Use of the software either in source or binary form, must be run
-#       on or directly connected to an Analog Devices Inc. component.
-#
-# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.
-#
-# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
-# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys
 print(f'sys.path = {sys.path}')       # Edit JB: may need to add path to PYTHONPATH for OSError: [Errno 16] Device or resource busy
@@ -75,24 +32,25 @@ print(f'sys.path = {sys.path}')       # Edit JB: may need to add path to PYTHONP
 import adi
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 '''Setup'''
 samp_rate = 2e6    # must be <=30.72 MHz if both channels are enabled
 NumSamples = 2**12
 rx_lo = 2.3e9
 rx_mode = "manual"  # can be "manual" or "slow_attack"
-rx_gain0 = 40
-rx_gain1 = 40
+rx_gain0 = 60
+rx_gain1 = 60
 tx_lo = rx_lo
-tx_gain = -5
+tx_gain = 0
 fc0 = int(200e3)
 phase_cal = 0
-num_scans = 5
+num_scans = 500
 
 ''' Set distance between Rx antennas '''
-d_wavelength = 0.5                  # distance between elements as a fraction of wavelength.  This is normally 0.5
-wavelength = 3E8 / rx_lo              # wavelength of the RF carrier
-d = d_wavelength*wavelength         # distance between elements in meters
+d_wavelength = 0.5                      # distance between elements as a fraction of wavelength.  This is normally 0.5
+wavelength = 3E8 / rx_lo                # wavelength of the RF carrier
+d = d_wavelength * wavelength           # distance between elements in meters
 print("Set distance between Rx Antennas to ", int(d*1000), "mm")
 
 '''Create Radios'''
@@ -101,7 +59,7 @@ sdr = adi.ad9361(uri='ip:192.168.2.1')
 '''Configure properties for the Rx Pluto'''
 sdr.rx_enabled_channels = [0, 1]
 sdr.sample_rate = int(samp_rate)
-sdr.rx_rf_bandwidth = int(fc0*3)
+sdr.rx_rf_bandwidth = int(fc0 * 3)
 sdr.rx_lo = int(rx_lo)
 sdr.gain_control_mode = rx_mode
 sdr.rx_hardwaregain_chan0 = int(rx_gain0)
@@ -128,13 +86,13 @@ sdr.tx([iq0,iq0])  # Send Tx data.
 # Assign frequency bins and "zoom in" to the fc0 signal on those frequency bins
 xf = np.fft.fftfreq(NumSamples, ts)
 xf = np.fft.fftshift(xf)/1e6
-signal_start = int(NumSamples*(samp_rate/2+fc0/2)/samp_rate)
-signal_end = int(NumSamples*(samp_rate/2+fc0*2)/samp_rate)
+signal_start = int(NumSamples * (samp_rate / 2 + fc0 / 2) / samp_rate)
+signal_end = int(NumSamples * (samp_rate / 2 + fc0 * 2) / samp_rate)
 
 def calcTheta(phase):
     # calculates the steering angle for a given phase delta (phase is in deg)
     # steering angle is theta = arcsin(c*deltaphase/(2*pi*f*d)
-    arcsin_arg = np.deg2rad(phase)*3E8/(2*np.pi*rx_lo*d)
+    arcsin_arg = np.deg2rad(phase) * 3E8 / (2 * np.pi * rx_lo * d)
     arcsin_arg = max(min(1, arcsin_arg), -1)     # arcsin argument must be between 1 and -1, or numpy will throw a warning
     calc_theta = np.rad2deg(np.arcsin(arcsin_arg))
     return calc_theta
@@ -146,7 +104,7 @@ def dbfs(raw_data):
     y = raw_data * win
     s_fft = np.fft.fft(y) / np.sum(win)
     s_shift = np.fft.fftshift(s_fft)
-    s_dbfs = 20*np.log10(np.abs(s_shift)/(2**11))     # Pluto is a signed 12 bit ADC, so use 2^11 to convert to dBFS
+    s_dbfs = 20 * np.log10(np.abs(s_shift)/(2**11))     # Pluto is a signed 12 bit ADC, so use 2^11 to convert to dBFS
     return s_shift, s_dbfs
 
 def monopulse_angle(array1, array2):
@@ -163,14 +121,14 @@ def monopulse_angle(array1, array2):
 def scan_for_DOA():
     # go through all the possible phase shifts and find the peak, that will be the DOA (direction of arrival) aka steer_angle
     data = sdr.rx()
-    Rx_0=data[0]
-    Rx_1=data[1]
+    Rx_0 = data[0]
+    Rx_1 = data[1]
     peak_sum = []
     peak_delta = []
     monopulse_phase = []
     delay_phases = np.arange(-180, 180, 2)    # phase delay in degrees
     for phase_delay in delay_phases:   
-        delayed_Rx_1 = Rx_1 * np.exp(1j*np.deg2rad(phase_delay+phase_cal))
+        delayed_Rx_1 = Rx_1 * np.exp(1j * np.deg2rad(phase_delay + phase_cal))
         delayed_sum = Rx_0 + delayed_Rx_1
         delayed_delta = Rx_0 - delayed_Rx_1
         delayed_sum_fft, delayed_sum_dbfs = dbfs(delayed_sum)
@@ -182,7 +140,7 @@ def scan_for_DOA():
         monopulse_phase.append(np.sign(mono_angle))
         
     peak_dbfs = np.max(peak_sum) # translate phase difference to 
-    peak_delay_index = np.where(peak_sum==peak_dbfs)
+    peak_delay_index = np.where(peak_sum == peak_dbfs)
     peak_delay = delay_phases[peak_delay_index[0][0]]
     steer_angle = int(calcTheta(peak_delay))
     
@@ -194,20 +152,22 @@ for i in range(20):
     data = sdr.rx()
     
 for i in range(num_scans):
-    
     delay_phases, peak_dbfs, peak_delay, steer_angle, peak_sum, peak_delta, monopulse_phase = scan_for_DOA()
-
+    plt.clf()
     plt.plot(delay_phases, peak_sum)
     plt.plot(delay_phases, peak_delta)
     plt.plot(delay_phases, monopulse_phase)
-    plt.axvline(x=peak_delay, color='r', linestyle=':')
+    plt.axvline(x = peak_delay, color='r', linestyle=':')
     plt.text(-180, -26, "Peak signal occurs with phase shift = {} deg".format(round(peak_delay,1)))
-    plt.text(-180, -28, "If d={}mm, then steering angle = {} deg".format(int(d*1000), steer_angle))
-    plt.ylim(top=5, bottom=-30)        
+    plt.text(-180, -28, "If d={}mm, then steering angle = {} deg".format(int(d * 1000), steer_angle))
+    plt.ylim(top=5, bottom=-60)        
     plt.xlabel("phase shift [deg]")
     plt.ylabel("Rx0 + Rx1 [dBfs]")
     plt.draw()
-    plt.show()
-        
+    plt.pause(0.05)
+    time.sleep(0.1)
+    # plt.show()
+plt.show()
+
 sdr.tx_destroy_buffer()
-if i>40: print('\a')    # for a long capture, beep when the script is done
+if i > 40: print('\a')    # for a long capture, beep when the script is done
