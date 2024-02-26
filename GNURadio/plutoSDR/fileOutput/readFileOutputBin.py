eee2826f21545fecc13bd6d5118b22a62c4e6942
Joel Brigida <joel@drtechreps.com>
2024-02-25 22:23:15 -0500

    Add skeleton for time domain plot

--- GNURadio/plutoSDR/fileOutput/readFileOutputBin.py
+++ GNURadio/plutoSDR/fileOutput/readFileOutputBin.py
@@ -21,7 +21,8 @@ def dbfs(raw_data):
     return s_dbfs
 
 ''' File names go here '''
-FILE_1 = "GNURadio/fileOutput"
+# FILE_1 = "GNURadio/fileOutput"
+FILE_1 = "GNURadio/plutoSDR/fileOutput/fileOutput"
 # FILE_2 = "etc..."
 
 '''Extract data as a complex64'''
@@ -32,29 +33,42 @@ f1 = np.fromfile(open(FILE_1), dtype=np.complex64)
 '''Visualizing the data in the frequency domain'''
 SAMPLE_RATE = 2e6  # should be the same as it was in GNU Radio
 NumSamples = f1.shape[0] # this ensures that it is relative to what is captured
-fs = int(SAMPLE_RATE)
-ts = 1 / float(fs)
+fs = int(SAMPLE_RATE)                       # frequency size
+ts = 1 / float(fs)                          # time size
 xf = np.fft.fftfreq(NumSamples, ts)         # Assign frequency bins
-xf = np.fft.fftshift(xf)/1e6
+xf = np.fft.fftshift(xf) / 1e6              # Convert to MHz
+
+# '''Visualize Frequency Drift in the Time Domain'''
+# signal, sample_rate = librosa.load(blues_1)
+# max_time = signal.size / sample_rate
 
 '''QT does not accept complex data, so we convert IQ samples to FFT'''
-f1 = dbfs(f1)
+f1_db = dbfs(f1)
 # f2 = dbfs(f2)
 
 '''Create a main QT Window'''
 win = pg.GraphicsLayoutWidget(show=True, size=(1200, 600), title="File Output")
 p1 = win.addPlot()
-p1.setLabel('bottom', 'Frequency', '[MHz]', **{'color': '#FFF', 'size': '14pt'})
-p1.setLabel('left', 'Relative Gain', '[dBfs]', **{'color': '#FFF', 'size': '14pt'})
+p1.setLabel('bottom', 'Frequency', 'MHz', **{'color': '#FFF', 'size': '14pt'})
+p1.setLabel('left', 'Relative Gain', 'dBfs', **{'color': '#FFF', 'size': '14pt'})
+# p2 = win.addplot()
+# p2.setLabel('bottom', 'Time', 'seconds', **{'color': '#FFF', 'size': '14pt'})
+# p2.setLabel('left', 'Frequency', 'Hz', **{'color': '#FFF', 'size': '14pt'})
 
 '''Per each data stream, create a curve on the same p1 plot'''
 # Change the pen to any other color for clarity - 'b' is blue
-curve1 = p1.plot(pen=pg.mkPen('b'))
-curve1.setData(xf, f1)
+curve1 = p1.plot(pen=pg.mkPen('r'))
+curve1.setData(xf, f1_db)
 label1 = pg.TextItem("Pluto 1 in BLUE")
 label1.setParentItem(p1)
 label1.setPos(65, 2)
-# curve2 = p2.plot(pen=pg.mkPen('r'))
+
+# time_steps = np.linspace(0, max_time, signal.size)
+# plt.figure()
+# plt.plot(time_steps, signal)
+# plt.xlabel("Time [s]")
+# plt.ylabel("Amplitude")
+# curve2 = p2.plot(pen=pg.mkPen('b'))
 # curve2.setData(xf, f2)
 # label2 = pg.TextItem("Pluto 2 in RED")
 # label2.setParentItem(p1)
