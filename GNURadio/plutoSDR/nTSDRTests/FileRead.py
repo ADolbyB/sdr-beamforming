a702b791b1b1047071abe4f5aa5632c9216402ac
RayzrReptile <padkins2020@fau.edu>
2024-03-15 22:15:26 -0400

    nTSDR Test Tools

--- GNURadio/plutoSDR/nTSDRTests/FileRead.py
+++ GNURadio/plutoSDR/nTSDRTests/FileRead.py
@@ -12,29 +12,6 @@ from pylab import *
 import pyqtgraph as pg  
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 
-'''Function to convert IQ samples to FFT plot, scaled in dBFS - Kraft'''
-def dbfs(raw_data):                         
-    NumSamples = len(raw_data)
-    win = np.hamming(NumSamples)
-    y = raw_data * win
-    s_fft = np.fft.fft(y) / np.sum(win)
-    s_shift = np.fft.fftshift(s_fft)
-    s_dbfs = 20*np.log10(np.abs(s_shift)/(2**11))   # Pluto is a signed 12 bit ADC, so use 2^11 to convert to dBFS
-    return s_dbfs
-
-'''Function for trimming ndarray data'''
-def trimDelay(input, delayDelta):
-    input = np.pad(input, (0, delayDelta), 'constant', constant_values=(0))
-    input = input[delayDelta:]
-    return input
-
-'''Function for padding ndarray data'''
-def padDelay(input, delayDelta):
-    length = len(input)
-    input = np.pad(input, (delayDelta, 0), 'constant', constant_values=(0))
-    input = input[:length]
-    return input
-
 '''Function for corss-correlation - Krysik'''
 def xcorrelate(X,Y,maxlag):
     N = max(len(X), len(Y))
@@ -57,7 +34,7 @@ def xcorrelate(X,Y,maxlag):
     return R
 
 '''Function for computing and finding delays - Krysik'''
-def compute_and_set_delay(ref_data, Rx_data, Rx_name, samp_rate):
+def compute_and_find_delay(ref_data, Rx_data, Rx_name, samp_rate):
 
     result_corr = xcorrelate(ref_data, Rx_data,int(len(ref_data)/2))
     max_position = np.argmax(abs(result_corr))
@@ -69,44 +46,38 @@ def compute_and_set_delay(ref_data, Rx_data, Rx_name, samp_rate):
 
     print ("Delay of ", Rx_name, ": ", delay,' | Phase Diff: ', phase_diff, " [deg]")
 
-    # Set phase amplitude correction
-    # INSERT
-    # phase_amplitude_correction = sqrt(var(ref_data)/var(Rx_data))*(exp(1j*angle(phase_amplitude_correction)))
-
-    # Set delay     
-    # if delay < 0:
-    #     return trimDelay(Rx_data, int(-delay)) # *samp_rate
-    # elif delay > 0:
-    #     return padDelay(Rx_data, int(delay)) # *samp_rate
-    # else:
-    #     return Rx_data
-
-    # return Rx_data * np.exp(1j*np.deg2rad(phase_diff))
-    return Rx_data * (np.exp(1j*np.deg2rad(phase_diff)))
-    
+'''Function to convert IQ samples to FFT plot, scaled in dBFS - Kraft'''
+def dbfs(raw_data):                         
+    NumSamples = len(raw_data)
+    win = np.hamming(NumSamples)
+    y = raw_data * win
+    s_fft = np.fft.fft(y) / np.sum(win)
+    s_shift = np.fft.fftshift(s_fft)
+    s_dbfs = 20*np.log10(np.abs(s_shift)/(2**11))   # Pluto is a signed 12 bit ADC, so use 2^11 to convert to dBFS
+    return s_dbfs
 
 ''' File names go here '''
-FILE_Rx1 = "/home/ubuntu/PlutoSDR/sdr-beamforming/GNURadio/plutoSDR/LocalPhaseSync/fileOutputRx1"
-FILE_Rx2 = "/home/ubuntu/PlutoSDR/sdr-beamforming/GNURadio/plutoSDR/LocalPhaseSync/fileOutputRx2"
+FILE_Rx1 = "/home/ubuntu/nTSDR_Local_Testing/fileOutputRx1"
+FILE_Rx2 = "/home/ubuntu/nTSDR_Local_Testing/fileOutputRx2"
 
 '''Extract data as a complex64'''
 # This is the stream output for basic signal processing in GNU Radio
 fRx1 = fromfile(open(FILE_Rx1), dtype=np.complex64)
 fRx2 = fromfile(open(FILE_Rx2), dtype=np.complex64)
 fRx2 = resize(fRx2, fRx1.shape) # resize to match first Rx node
-print(fRx1.shape)
+# Trim to only look at second half - ensures only adjusted values
+# fRx1 = fRx1[math.floor(len(fRx1)/2):len(fRx1)]
+# fRx2 = fRx2[math.floor(len(fRx2)/2):len(fRx2)]
 
 DOMAIN = "time" # freq or time
 SAMPLE_RATE = 2e6  # should be the same as it was in GNU Radio
 NUM_SAMPLES = fRx1.shape[0] # this ensures that it is relative to what is captured
 
-'''Cross-Correlation and Delay Values'''
-# DfRx2 = fRx2
-DfRx2 = compute_and_set_delay(fRx1, fRx2, "Rx2", SAMPLE_RATE)
-DfRx2 = compute_and_set_delay(fRx1, DfRx2, "Rx2", SAMPLE_RATE)
+'''Find Phase and Delay'''
+compute_and_find_delay(fRx1, fRx2, "Rx_2", SAMPLE_RATE)
 
 '''Create a main QT Window'''
-win_raw = pg.GraphicsLayoutWidget(show=True, size=(1200, 600), title="File Output")
+win_raw = pg.GraphicsLayoutWidget(show=True, size=(1200, 600), title="Raw File Output")
 
 '''Visualizing the raw data'''
 
@@ -120,7 +91,7 @@ if DOMAIN == "freq":
 
     # QT does not accept complex data, so we convert IQ samples to FFT
     fRx1_db = dbfs(fRx1)
-    fRx2_db = dbfs(DfRx2)
+    fRx2_db = dbfs(fRx2)
 
     # Visualize data
     p1 = win_raw.addPlot()
@@ -143,13 +114,12 @@ elif DOMAIN == "time":
 
     # QT does not accept complex data, so we separate IQ samples by their real values from their imaginary values
     fRx1_r = real(fRx1)
-    fRx2_r_raw = real(fRx2)
-    fRx2_r_adj = real(DfRx2)
+    fRx2_r = real(fRx2)
 
     # Time axis
     taxs = arange(NUM_SAMPLES)/SAMPLE_RATE
     
-    # Visualize raw data
+    # Visualize data
     p1 = win_raw.addPlot()
     p1.setLabel('bottom', 'Time', 'sec', **{'color': '#FFF', 'size': '14pt'})
     p1.setLabel('left', 'Amplitude', **{'color': '#FFF', 'size': '14pt'})
@@ -162,27 +132,9 @@ elif DOMAIN == "time":
     label1.setParentItem(p1)
     label1.setPos(65, 2)
     curve2 = p1.plot(pen=pg.mkPen('r'))
-    curve2.setData(taxs, fRx2_r_raw)
-    label2 = pg.TextItem("Rx2 (raw) in RED")
+    curve2.setData(taxs, fRx2_r)
+    label2 = pg.TextItem("Rx2 in RED")
     label2.setParentItem(p1)
-    label2.setPos(65, 24) # Change Y position for each label
-
-    # Visualize synced data
-    p2 = win_raw.addPlot()
-    p2.setLabel('bottom', 'Time', 'sec', **{'color': '#FFF', 'size': '14pt'})
-    p2.setLabel('left', 'Amplitude', **{'color': '#FFF', 'size': '14pt'})
-    p2.setYRange(-2, 2, padding=0)
-    # p1.setXRange(0, 0.25, padding=0)
-    # Change the pen to any other color for clarity - 'b' is blue
-    curve1 = p2.plot(pen=pg.mkPen('b'))
-    curve1.setData(taxs, fRx1_r)
-    label1 = pg.TextItem("Rx1 in BLUE")
-    label1.setParentItem(p2)
-    label1.setPos(65, 2)
-    curve2 = p2.plot(pen=pg.mkPen('r'))
-    curve2.setData(taxs, fRx2_r_adj)
-    label2 = pg.TextItem("Rx2 (adjusted) in RED")
-    label2.setParentItem(p2)
     label2.setPos(65, 24) # Change Y position for each label
     
 else: 
