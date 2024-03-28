604cc1463e138b9db0bc8cd854332524a07ca69b
Joel Brigida <joel@drtechreps.com>
2024-03-27 21:55:22 -0400

    Added Raster Plot Type & Minor Syntax Updates

--- GNURadio/plutoSDR/dualPlutoFileSink/demoFileRead.py
+++ GNURadio/plutoSDR/dualPlutoFileSink/demoFileRead.py
@@ -1,18 +1,20 @@
 '''
     Peyton Adkins - Joel Brigida
-    This is a Python script that reads data from the specified binary folders of Rx1 and Rx2 data sourced from a single Pluto SDR. This script is derived from the logic of both Jon Kraft's work and Piotr Krysik's work in writing Python code for the Pluto SDR and for synchronizing multiple SDRs in GNU Radio.
+    This is a Python script that reads data from the specified binary folders of Rx1 and Rx2 data sourced from a single 
+    Pluto SDR. This script is derived from the logic of both Jon Kraft's work and Piotr Krysik's work in writing Python 
+    code for the Pluto SDR and for synchronizing multiple SDRs in GNU Radio.
 
-    The actual data for the stream output does not indicate any relatice domain. For this script, it is written to be displayed in the time domain to help aid in confirming phase lock across multiple Rx nodes of a single Pluto SDR.
+    The actual data for the stream output does not indicate any relatice domain. For this script, it is written to be 
+    displayed in the time domain to help aid in confirming phase lock across multiple Rx nodes of a single Pluto SDR.
 '''
 
-import math
-from numpy import *
+#import math
 import numpy as np
-from pylab import *
+import pylab as pl
 import pyqtgraph as pg  
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 
-'''Function to convert IQ samples to FFT plot, scaled in dBFS - Kraft'''
+''' Function to convert IQ samples to FFT plot, scaled in dBFS - Kraft '''
 def dbfs(raw_data):                         
     NumSamples = len(raw_data)
     win = np.hamming(NumSamples)
@@ -23,36 +25,37 @@ def dbfs(raw_data):
     return s_dbfs
 
 ''' File names go here '''
-FILE_P2 = "/home/ubuntu/fileOutputP2"
-FILE_P3 = "/home/ubuntu/fileOutputP3"
-FILE_P4 = "/home/ubuntu/fileOutputP4"
-FILE_P5 = "/home/ubuntu/fileOutputP5"
+FILE_P2 = "/home/sdr/code/sdr-beamforming/GNURadio/plutoSDR/dualPlutoFileSink/fileOutputP2.iq"
+FILE_P3 = "/home/sdr/code/sdr-beamforming/GNURadio/plutoSDR/dualPlutoFileSink/fileOutputP3.iq"
+FILE_P4 = "/home/sdr/code/sdr-beamforming/GNURadio/plutoSDR/dualPlutoFileSink/fileOutputP4.iq"
+FILE_P5 = "/home/sdr/code/sdr-beamforming/GNURadio/plutoSDR/dualPlutoFileSink/fileOutputP5.iq"
 
 '''Extract data as a complex64'''
 # This is the stream output for basic signal processing in GNU Radio
-fP2 = fromfile(open(FILE_P2), dtype=np.complex64)
-fP3 = fromfile(open(FILE_P3), dtype=np.complex64)
-fP3 = resize(fP3, fP2.shape)
-fP4 = fromfile(open(FILE_P4), dtype=np.complex64)
-fP4 = resize(fP4, fP2.shape)
-fP5 = fromfile(open(FILE_P5), dtype=np.complex64)
-fP5 = resize(fP5, fP2.shape)
+fP2 = pl.fromfile(open(FILE_P2), dtype=np.complex64)
+fP3 = pl.fromfile(open(FILE_P3), dtype=np.complex64)
+fP3 = pl.resize(fP3, fP2.shape)
+fP4 = pl.fromfile(open(FILE_P4), dtype=np.complex64)
+fP4 = pl.resize(fP4, fP2.shape)
+fP5 = pl.fromfile(open(FILE_P5), dtype=np.complex64)
+fP5 = pl.resize(fP5, fP2.shape)
 
+GRAPHS = "raster" # stack, raster or all
 DOMAIN = "freq" # freq or time
 SAMPLE_RATE = 2e6  # should be the same as it was in GNU Radio
 NUM_SAMPLES = fP2.shape[0] # this ensures that it is relative to what is captured
 
-'''Create a main QT Window'''
+''' Create a main QT Window '''
 win_raw = pg.GraphicsLayoutWidget(show=True, size=(1200, 600), title="Raw File Output")
 
-'''Visualizing the raw data'''
-
-if DOMAIN == "freq":
-    # # # Frequency Domain
+''' Visualizing raw data '''
+if DOMAIN == "freq" and GRAPHS == "stack":
+    ## Frequency Domain
+    # 1 graph with all plots stacked on top of each other
 
     fs = int(SAMPLE_RATE)                       # frequency size
     ts = 1 / float(fs)                          # time size
-    xf = np.fft.fftfreq(NUM_SAMPLES, ts)         # Assign frequency bins
+    xf = np.fft.fftfreq(NUM_SAMPLES, ts)        # Assign frequency bins
     xf = np.fft.fftshift(xf) / 1e6              # Convert to MHz
 
     # QT does not accept complex data, so we convert IQ samples to FFT
@@ -87,6 +90,57 @@ if DOMAIN == "freq":
     label4.setParentItem(p1)
     label4.setPos(65, 68) # Change Y position for each label
 
+elif DOMAIN == "freq" and GRAPHS == "raster":
+    ## Frequency Domain
+    # 1 window with different graph plots for each channel
+
+    fs = int(SAMPLE_RATE)                       # frequency size
+    ts = 1 / float(fs)                          # time size
+    xf = np.fft.fftfreq(NUM_SAMPLES, ts)         # Assign frequency bins
+    xf = np.fft.fftshift(xf) / 1e6              # Convert to MHz
+
+    # QT does not accept complex data, so we convert IQ samples to FFT
+    fRx1_db = dbfs(fP2)
+    fRx2_db = dbfs(fP3)
+    fRx3_db = dbfs(fP4)
+    fRx4_db = dbfs(fP5)
+
+    # Visualize data
+    p1 = win_raw.addPlot()
+    p1.setLabel('bottom', 'Frequency', 'MHz', **{'color': '#FFF', 'size': '14pt'})
+    p1.setLabel('left', 'Relative Gain - Rx1', 'dBfs', **{'color': '#FFF', 'size': '14pt'})
+    p2 = win_raw.addPlot()
+    p2.setLabel('bottom', 'Frequency', 'MHz', **{'color': '#FFF', 'size': '14pt'})
+    p2.setLabel('left', 'Relative Gain - Rx2', 'dBfs', **{'color': '#FFF', 'size': '14pt'})
+    p3 = win_raw.addPlot()
+    p3.setLabel('bottom', 'Frequency', 'MHz', **{'color': '#FFF', 'size': '14pt'})
+    p3.setLabel('left', 'Relative Gain - Rx3', 'dBfs', **{'color': '#FFF', 'size': '14pt'})
+    p4 = win_raw.addPlot()
+    p4.setLabel('bottom', 'Frequency', 'MHz', **{'color': '#FFF', 'size': '14pt'})
+    p4.setLabel('left', 'Relative Gain - Rx4', 'dBfs', **{'color': '#FFF', 'size': '14pt'})
+    # Change the pen to any other color for clarity - 'b' is blue
+    curve1 = p1.plot(pen=pg.mkPen('b'))
+    curve1.setData(xf, fRx1_db)
+    label1 = pg.TextItem("Pluto 2.1 Rx1 in BLUE")
+    label1.setParentItem(p1)
+    label1.setPos(65, 2)
+    curve2 = p2.plot(pen=pg.mkPen('r'))
+    curve2.setData(xf, fRx2_db)
+    label2 = pg.TextItem("Pluto 2.1 Rx2 in RED")
+    label2.setParentItem(p1)
+    label2.setPos(65, 24) # Change Y position for each label
+    curve3 = p3.plot(pen=pg.mkPen('g'))
+    curve3.setData(xf, fRx3_db)
+    label3 = pg.TextItem("Pluto 3.1 Rx1 in GREEN")
+    label3.setParentItem(p1)
+    label3.setPos(65, 46) # Change Y position for each label
+    curve4 = p4.plot(pen=pg.mkPen('y'))
+    curve4.setData(xf, fRx4_db)
+    label4 = pg.TextItem("Pluto 3.1 Rx2 in YELLOW")
+    label4.setParentItem(p1)
+    label4.setPos(65, 68) # Change Y position for each label
+    
+
 # elif DOMAIN == "time":
 #     # # # Time Domain
 
@@ -116,11 +170,10 @@ if DOMAIN == "freq":
 #     label2.setPos(65, 24) # Change Y position for each label
     
 else: 
-    raise ValueError(print("Not a valid domain type."))
+    raise ValueError(print("Not a valid domain/graph type."))
 
 # Keep QT Graph open until exiting
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        # QtGui.QApplication.instance().exec_() # 2023-12-01 JB: does not work (see next line)
         QtGui.QGuiApplication.instance().exec()
